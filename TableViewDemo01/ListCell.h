fc301c5c52e87961790e5c64ab842122798ec055
fatbigbright <bigbright@gmail.com>
2012-09-11 14:23:29 +0800

    终于能显示tableview里的数据了，下一步就是push 到详细视图了。

---
+++ TableViewDemo01/ListCell.h
@@ -0,0 +1,15 @@
+//
+//  ListCell.h
+//  TableViewDemo01
+//
+//  Created by 亮 常 on 12-9-11.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListCell : UITableViewCell
+
+@property (strong, nonatomic) IBOutlet UILabel *item;
+
+@end
---
+++ TableViewDemo01/ListViewController.h
@@ -0,0 +1,15 @@
+//
+//  ListViewController.h
+//  TableViewDemo01
+//
+//  Created by 亮 常 on 12-9-11.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListViewController : UITableViewController
+
+@property (strong, nonatomic) NSArray *list;
+
+@end
